6ee0e0d991738a74661a78b9dfe5615f66368ac6
jhoun <jayhoun@gmail.com>
2017-10-03 11:27:10 -1000

    added instructions in aboutme.js

---
+++ aboutme.js
@@ -0,0 +1,91 @@
+// Create sanity check below
+
+
+
+// console.log each variable
+// follow example below (remove "//" to uncomment)
+
+// var school = "Kapolei Middle School";
+// console.log(school); <== this should print "Kapolei Middle School" in your browser
+
+
+// # 1
+// Variables with a "String" value
+// Declare the following variables name:
+// firstName
+// lastName
+// birthPlace
+// favFood
+// favDrink
+// favSong
+// favAnimal
+// favColor
+// favSport
+// favDoughnut
+
+
+
+
+
+
+
+// #2
+// Variables with a Number value
+// Declare the following variable names:
+// favNumber
+// yourShoeSize
+// numberOfPets
+// numberOfSiblings
+// numberOfStudentsInClass
+// numberOfDoorsInClassroom
+
+
+
+
+
+
+// #3
+// Variables with a Boolean value
+// Declare the following variable names:
+// likesMcDonalds
+// eatsDoughnuts
+// ownsRedShoes
+// hasAPet
+// playThePiano
+// playSports
+
+
+
+
+
+
+// #4
+// Variables with a Null value
+// Declare the following variable names:
+// completedMiddleSchool
+// traveledToMars
+// ateDinner
+
+
+
+
+
+
+// #5
+// Variables with an undefined value
+// Declare the following variable names:
+// superBowlChamps
+// lastDayOfSchool
+// nextFieldTrip
+
+
+
+
+
+
+// #6
+// Variables with an Array value
+// Declare a variable named `backpack and assign it an array containing 5 items in your backpack.
+// Declare a variable named plateLunch and assign it an array containing 5 of your favorite lunch items.
+// Declare a variable named `donutBox` and assign it an array containing 5 donuts of your choosing.
+// Declare a variable named `pizzaToppings` and assign it an array containing 5 pizza toppings of your choosing.
